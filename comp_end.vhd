e1830c506c7b1fd3789fbe3ae5f53a5cb66a5450
emanuelle guse <120527386+manuguse@users.noreply.github.com>
2023-06-21 18:46:41 -0400

    Update comp_end.vhd

--- comp_end.vhd
+++ comp_end.vhd
@@ -4,7 +4,7 @@ use IEEE.std_logic_1164.all;
 use IEEE.numeric_std.all;
 
 entity comp_end is port (
-    E0: in std_logic_vector(14 downto 0);
+    E0: in std_logic_vector(3 downto 0);
     endgame: out std_logic
 );
 end comp_end;
@@ -13,7 +13,7 @@ architecture comp_end of comp_end is
 begin 
     process(E0)
     begin
-    if E0 = "000000000000000" then
+    if E0 = "0000" then
         endgame <= '1';
     else
         endgame <= '0';
